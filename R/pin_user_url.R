81089d86a07b30c5e6efbb5b1b0e1f44afa60418
Camila Achury <camilaachury.4@gmail.com>
2020-08-05 17:24:43 -0300

    url for element saved by pin

---
+++ R/pin_user_url.R
@@ -0,0 +1,10 @@
+#' @export
+pin_user_url <- function(title, element, user_id, user_name) {
+  Sys.setlocale(locale = "en_US.UTF-8")
+  dv <- dsviz(element, name = title)
+  dspins_user_board_connect(user_id)
+  pin_url <- pin(dv, user_id = user_id)
+  url <-  paste0(user_name, ".datasketch.co/", title)
+  if (is.null(pin_url)) url <- "pinnotfound"
+  url
+}
