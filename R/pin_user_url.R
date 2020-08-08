7383af5a3c7d8ee8a91639d68a5de309275e1abb
Camila Achury <camilaachury.4@gmail.com>
2020-08-07 21:21:53 -0300

    add reactive in pin user url

--- R/pin_user_url.R
+++ R/pin_user_url.R
@@ -1,10 +1,14 @@
 #' @export
 pin_user_url <- function(title, element, user_id, user_name) {
+  if (is.reactive(title)) title <- title()
+  if (is.reactive(element)) element <- element()
+
   Sys.setlocale(locale = "en_US.UTF-8")
   dv <- dsviz(element, name = title)
   dspins_user_board_connect(user_id)
   pin_url <- pin(dv, user_id = user_id)
-  url <-  paste0(user_name, ".datasketch.co/", title)
-  if (is.null(pin_url)) url <- "pinnotfound"
+  url <- paste0(user_name, ".datasketch.co/", title)
+  if (is.null(pin_url))
+    url <- "pinnotfound"
   url
 }
