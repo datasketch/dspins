909bd67f556a36ff22910653ce1263bb9bbfa613
Lena Mangold <lena@mangold.org>
2021-01-19 17:03:32 -0500

    update user_bucket_create function

--- R/create_user_board.R
+++ R/create_user_board.R
@@ -5,9 +5,9 @@ user_board_create <- function(bucket_id, env_file = ".env"){
 
   # load_env()
 
-  if(!user_bucket_exists(bucket_id)){
+  if(!user_folder_exists(bucket_id)){
     message("No bucket exists for this user. Creating bucket.")
-    created <- user_bucket_create(bucket_id)
+    created <- user_folder_create(bucket_id)
     message("Bucket created: ",paste0("dskt.ch.",bucket_id), created)
     if(!created)
       stop("Something wrong creating the bucket")
@@ -19,15 +19,28 @@ user_board_create <- function(bucket_id, env_file = ".env"){
 }
 
 user_bucket_exists <- function(username){
+  .Deprecated("user_folder_exists")
+  # load_env()
+  aws.s3::object_exists(object = username, bucket = "user.dskt.ch")
+}
+
+user_folder_exists <- function(username){
   # load_env()
   aws.s3::object_exists(object = username, bucket = "user.dskt.ch")
 }
 
 user_bucket_create <- function(username){
+  .Deprecated("user_folder_create")
   # load_env()
-  aws.s3::put_bucket(paste0("dskt.ch.", username),
-                     region = "us-east-1"
-                     #headers = list(`x-amz-acl` = "public-read")
-                     )
+  aws.s3::put_folder(username,
+                     bucket = "user.dskt.ch"
+  )
+}
+
+user_folder_create <- function(username){
+  # load_env()
+  aws.s3::put_folder(username,
+                     bucket = "user.dskt.ch"
+  )
 }
 
