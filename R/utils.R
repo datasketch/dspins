6c6b7035b368405b6d24cbc45ce9aa38263bf15c
Lena Mangold <lena@mangold.org>
2021-01-19 17:33:23 -0500

    update change_content_type function

--- R/utils.R
+++ R/utils.R
@@ -74,7 +74,7 @@ is.url <- function(x){
 
 
 
-change_content_type <- function(slug, bucket_id, format){
+change_content_type <- function(slug, bucket_id, format, board = "user.dskt.ch"){
 
   content_type <- list(csv = "text/csv",
                        html = "text/html",
@@ -82,8 +82,8 @@ change_content_type <- function(slug, bucket_id, format){
                        png = "image/png",
                        svg = "image/svg+xml")
 
-  aws.s3::copy_object(glue::glue("{slug}/{slug}.{format}"), glue::glue("{slug}/{slug}.{format}"),
-                      glue::glue("{bucket_id}.dskt.ch"), glue::glue("{bucket_id}.dskt.ch"),
+  aws.s3::copy_object(glue::glue("{bucket_id}/{slug}/{slug}.{format}"), glue::glue("{bucket_id}.{slug}/{slug}.{format}"),
+                      glue::glue(board), glue::glue(board),
                       headers = list(`Content-Type` = content_type[[format]],
                                      `x-amz-metadata-directive` = "REPLACE"))
 
