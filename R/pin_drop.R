c0abcef69a3bad632458eb0bc12ab543bfb1b766
Lena Mangold <lena@mangold.org>
2021-01-21 12:14:19 -0500

    update pin_drop function

--- R/pin_drop.R
+++ R/pin_drop.R
@@ -11,25 +11,25 @@ pin.drop <- function(drop, name = NULL, description = NULL, board = NULL, ...) {
 
   args <- list(...)
   bucket_id <- args$bucket_id
-  if(!is.null(bucket_id)){
-    board <- board_name(bucket_id)
-  } else{
-    stop("Need to provide a bucket_id")
+
+  if(is.null(bucket_id)){
+    stop("Need a bucket_id to save dsviz")
   }
 
-  # drop$files_path <- drop$files
+  board <- "user.dskt.ch"
+
   slug <- drop$slug
   format <- drop$format
   metadata$files <- list(list(path = glue::glue(paste0("{slug}.", format)),
                               format = format,
-                              url = glue::glue("https://s3.amazonaws.com/{bucket_id}.dskt.ch/{slug}/{slug}.{format}"))) %>%
+                              url = glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"))) %>%
     setNames(format)
 
 
   metadata$share <- list(list(link =  glue::glue("https://datasketch.co/{bucket_id}/{slug}"),
-                              permalink =  glue::glue("https://s3.amazonaws.com/{bucket_id}.dskt.ch/{slug}/{slug}.{format}"),
+                              permalink =  glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"),
                               embed =  paste0('<iframe src="',
-                                              glue::glue("https://s3.amazonaws.com/{bucket_id}.dskt.ch/{slug}/{slug}.{format}"),
+                                              glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"),
                                               '" frameborder=0 width="100%" height="400px"></iframe>'))) %>%
     setNames(format)
 
@@ -38,12 +38,12 @@ pin.drop <- function(drop, name = NULL, description = NULL, board = NULL, ...) {
 
   drop_write(drop, path)
 
-  if(!dspins_is_board_connected(args$bucket_id))
+  if(!dspins_is_board_connected("user"))
     stop("Board not connected. Run: dspins_user_board_connect(bucket_id)")
 
-  dspins_user_board_connect(bucket_id)
-  # upload_url <- paste0("https://s3.amazonaws.com/",file.path(board_name(bucket_id), drop$slug))
-  board_pin_store(board, path, drop$slug, drop$description, "drop",
+  name <- paste0(bucket_id,"/",slug)
+
+  board_pin_store(board, path, name, drop$description, "drop",
                   extract = FALSE,
                   metadata,...)
   # upload_url
