8f4fdb09b9069612cd993325ce2a63817ce9de09
Lena Mangold <lena@mangold.org>
2021-01-21 11:46:14 -0500

    update dspin_list function

--- R/dspin_list.R
+++ R/dspin_list.R
@@ -5,27 +5,12 @@ dspin_list <- function(bucket_id){
   # if(length(pin_list_from_bucket) > length(in_data_txt$name )){
   #   message()
   # }
-  l <-tryCatch(pin_find(board = board_name(bucket_id), extended = TRUE),
+  l <-tryCatch(pin_find(board = "user.dskt.ch", extended = TRUE),
            error=function(e) e, warning=function(w) w)
   if(inherits(l,"error")){
     message("No pins in board\n Error: ",
             paste0(l$call[1], l$message[1], sep = "\n"))
     return()
   }
-  l
+  l %>% filter(str_detect(path, paste0("^", bucket_id, "/")))
 }
-
-
-
-
-pin_list_from_bucket <- function(bucket_id){
-  data_txt_bucket <- aws.s3::get_object("data.txt", board_name(bucket_id))
-  data_txt <- rawToChar(data_txt_bucket)
-  data_txt <- yaml::yaml.load(data_txt)
-  bucket_files <- aws.s3::get_bucket_df(board_name(bucket_id))
-  l <- strsplit(bucket_files$Key, "/")
-  pins <- unlist(unique(lapply(l, `[`, 1)))
-  pins[pins != "data.txt"]
-}
-
-
