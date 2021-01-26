eb8d360ede0b3d8a169a039c343ab905ffcc1a79
Lena Mangold <lena@mangold.org>
2021-01-26 09:11:17 -0500

    add custom board initialize function

--- R/board_register.R
+++ R/board_register.R
@@ -10,7 +10,7 @@ board_register_s3_dspins <- function(folder = "",
                               ...) {
 
   name <- paste0(bucket, "/", folder)
-  board_register("s3",
+  board_register("s3_dspins",
                  name = name,
                  bucket = bucket,
                  key = key,
@@ -19,4 +19,43 @@ board_register_s3_dspins <- function(folder = "",
                  region = region,
                  path = path,
                  ...)
+}
+
+
+board_initialize.s3_dspins <- function(board,
+                                bucket = Sys.getenv("AWS_BUCKET"),
+                                key = Sys.getenv("AWS_ACCESS_KEY_ID"),
+                                secret = Sys.getenv("AWS_SECRET_ACCESS_KEY"),
+                                cache = NULL,
+                                host = "s3.amazonaws.com",
+                                ...) {
+  board$bucket <- bucket
+  if (nchar(bucket) == 0) stop("The 's3' board requires a 'bucket' parameter.")
+
+  if (nchar(key) == 0)  stop("The 's3' board requires a 'key' parameter.")
+  if (nchar(secret) == 0)  stop("The 's3' board requires a 'secret' parameter.")
+
+  # folder_name <- sub('.*/', '', board$name)
+  # s3_url <- paste0("https://", bucket, ".", host, "/",folder_name)
+
+  s3_url <- paste0("https://", board$bucket, ".", host)
+
+  # class(board) <- "s3"
+  # board$board <- "s3"
+
+  board_register_datatxt(name = board$name,
+                         url = s3_url,
+                         cache = cache,
+                         headers = pins:::s3_headers,
+                         needs_index = FALSE,
+                         key = key,
+                         secret = secret,
+                         bucket = bucket,
+                         connect = FALSE,
+                         browse_url = paste0("https://s3.console.aws.amazon.com/s3/buckets/", bucket, "/"),
+                         # browse_url = paste0("https://s3.console.aws.amazon.com/s3/buckets/", board$name, "/"),
+                         host = host,
+                         ...)
+
+  board_get(board$name)
 }
