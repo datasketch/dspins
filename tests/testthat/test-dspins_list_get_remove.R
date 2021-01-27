f0bdeae73ac850fa0847e83bd25e273ee72b048b
Lena Mangold <lena@mangold.org>
2021-01-27 11:19:57 -0500

    extend tests for dspins list get remove

--- tests/testthat/test-dspins_list_get_remove.R
+++ tests/testthat/test-dspins_list_get_remove.R
@@ -1,12 +1,15 @@
-test_that("dspins, list, get, remove", {
+test_that("dspins, list ", {
 
   #options(pins.verbose = FALSE)
 
-  bucket_id <- "test-dspins-do-not-delete"
+  bucket_id <- "testuser"
+  folder <- "test"
 
-  expect_true(dspins_user_board_connect("user"))
+  expect_true(dspins_user_board_connect(bucket_id, folder))
 
-  pinned_mtcars <- pin(mtcars, bucket_id = bucket_id)
+  #local
+  pinned_mtcars <- pin(mtcars)
+  pin_remove("mtcars", board = "local")
 
   library(homodatum)
   data <- data.frame(book = c("Black", "Red"), value = 1:2)
@@ -14,28 +17,54 @@ test_that("dspins, list, get, remove", {
                license = "CC-BY", date_created = unix_timestamp(),
               access = "private")
 
-  #pin(f) # local board by default
+  pinned_fringe <- pin(f, bucket_id = bucket_id, folder = folder)
 
-  pinned_fringe <- pin(f, bucket_id = bucket_id) # local board by default
+  expect_error(dspin_list(bucket_id = "testuser"), "Need a folder to retrieve list of pins.")
 
-  all_pins <- dspin_list(bucket_id)
-  pin_remove("mtcars", board = "local")
+  expect_message(dspin_list(folder = "test"), "No bucket_id specified. Using 'user.dskt.ch' by default.")
 
-  get <- dspin_get("some-file-2", bucket_id = bucket_id)
+  all_pins <- dspin_list(folder, bucket_id)
+
+  expect_true("some-file-2" %in% all_pins$path)
 
 })
 
-test_that(" remove ", {
+test_that("dspins, get ", {
 
-  bucket_id <- "test-dspins-do-not-delete"
+  library(hgchmagic)
+  title <- "Another viz"
+  viz <- hgchmagic::hgch_bar_Cat(tibble(a = c("a","b")))
+
+  bucket_id <- "testuser"
+  folder <- "test"
+
+  dv <- dsviz(viz, name = title)
+  pin_url <- pin(dv, folder = folder, bucket_id = bucket_id)
+  pin_url <- pin(dv, folder = folder)
+
+  expect_error(dspin_get("another-viz"), "Need a folder to retrieve pin.")
+
+  expect_message(dspin_get("another-viz", folder = folder), "No bucket_id specified. Using 'user.dskt.ch' by default.")
+
+  pins <- dspin_get("another-viz", folder = folder, bucket_id = bucket_id)
+
+})
+
+test_that("dspins, remove ", {
+
+  bucket_id <- "testuser"
+  folder <- "test"
+
   library(homodatum)
   current_title <- paste0("mtcars-remove")
   f <- fringe(mtcars, name = current_title)
 
-  pinned_f <- pin(f, bucket_id = bucket_id)
+  pinned_f <- pin(f, folder = folder, bucket_id = bucket_id)
 
-  dspin_remove(current_title, bucket_id = bucket_id)
+  expect_error(dspin_remove(current_title, bucket_id = bucket_id), "Need a folder to remove pin")
 
+  expect_error(dspin_remove(current_title, folder = folder), "Need a bucket_id to remove pin")
 
+  dspin_remove(current_title, folder = folder, bucket_id = bucket_id)
 
 })
