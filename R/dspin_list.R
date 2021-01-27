7741ddcf19b2d1e455e3a56e572e632b53fb1d61
Lena Mangold <lena@mangold.org>
2021-01-27 11:11:10 -0500

    update dspin_list

--- R/dspin_list.R
+++ R/dspin_list.R
@@ -1,17 +1,24 @@
 
 #' @export
-dspin_list <- function(bucket_id){
-  # in_data_txt <- pin_find(board = board_name(bucket_id))
-  # if(length(pin_list_from_bucket) > length(in_data_txt$name )){
-  #   message()
-  # }
-  l <-tryCatch(pin_find(board = "user.dskt.ch", extended = TRUE),
+dspin_list <- function(folder = NULL, bucket_id = NULL){
+
+  if(is.null(bucket_id)){
+    message("No bucket_id specified. Using 'user.dskt.ch' by default.")
+    bucket_id <- "user"
+  }
+
+  if(is.null(folder)){
+    stop("Need a folder to retrieve list of pins.")
+  }
+
+  board <- board_name(bucket_id, folder)
+
+  l <-tryCatch(pin_find(board = board, extended = TRUE),
            error=function(e) e, warning=function(w) w)
   if(inherits(l,"error")){
     message("No pins in board\n Error: ",
             paste0(l$call[1], l$message[1], sep = "\n"))
     return()
   }
-  # l %>% filter(str_detect(path, paste0("^", bucket_id, "/")))
-  l %>% dplyr::filter(grepl(paste0("^", bucket_id, "/"), path))
+  l
 }
