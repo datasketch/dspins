dfccc12f92db28eb9e52bdc26a3a71d9ed25f0b4
Lena Mangold <lena@mangold.org>
2021-01-27 10:52:46 -0500

    update pin_drop

--- R/pin_drop.R
+++ R/pin_drop.R
@@ -4,32 +4,39 @@ pin.drop <- function(drop, name = NULL, description = NULL, board = NULL, ...) {
   dir.create(path)
   on.exit(unlink(path, recursive = TRUE))
 
-  name <- create_slug(name) %||% drop$slug
+  args <- list(...)
+  folder <- args$folder
+  bucket_id <- args$bucket_id
+
+  if(is.null(bucket_id)){
+    message("No bucket_id specified. Using 'user.dskt.ch' by default.")
+    bucket_id <- "user"
+  }
+
+  if(is.null(folder)){
+    stop("Need a folder to save drop")
+  }
+
+  bucket <- bucket_name(bucket_id)
+  board <- board_name(bucket_id, folder)
+  slug <- drop$slug
+
+  name <- create_slug(name) %||% slug
   metadata <- drop
   metadata$name <- NULL
   metadata$description <- NULL
 
-  args <- list(...)
-  bucket_id <- args$bucket_id
-
-  if(is.null(bucket_id)){
-    stop("Need a bucket_id to save dsviz")
-  }
-
-  board <- "user.dskt.ch"
-
-  slug <- drop$slug
   format <- drop$format
   metadata$files <- list(list(path = glue::glue(paste0("{slug}.", format)),
                               format = format,
-                              url = glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"))) %>%
+                              url = glue::glue("https://s3.amazonaws.com/{bucket}/{folder}/{slug}/{slug}.{format}"))) %>%
     setNames(format)
 
 
-  metadata$share <- list(list(link =  glue::glue("https://datasketch.co/{bucket_id}/{slug}"),
-                              permalink =  glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"),
+  metadata$share <- list(list(link =  glue::glue("https://datasketch.co/{folder}/{slug}"),
+                              permalink =  glue::glue("https://s3.amazonaws.com/{bucket}/{folder}/{slug}/{slug}.{format}"),
                               embed =  paste0('<iframe src="',
-                                              glue::glue("https://s3.amazonaws.com/user.dskt.ch/{bucket_id}/{slug}/{slug}.{format}"),
+                                              glue::glue("https://s3.amazonaws.com/{bucket}/{folder}/{slug}/{slug}.{format}"),
                                               '" frameborder=0 width="100%" height="400px"></iframe>'))) %>%
     setNames(format)
 
@@ -38,15 +45,15 @@ pin.drop <- function(drop, name = NULL, description = NULL, board = NULL, ...) {
 
   drop_write(drop, path)
 
-  if(!dspins_is_board_connected("user"))
-    stop("Board not connected. Run: dspins_user_board_connect(bucket_id)")
+  if(!dspins_is_board_connected(bucket_id, folder))
+    stop("Board not connected. Run: dspins_user_board_connect(bucket_id, folder)")
 
-  name <- paste0(bucket_id,"/",slug)
-
-  board_pin_store(board, path, name, drop$description, "drop",
+  board_pin_store(board, path, slug, drop$description, "drop",
                   extract = FALSE,
                   metadata,...)
-  # upload_url
+
+  message("Saved pin")
+
   drop
 }
 
