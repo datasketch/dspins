9243381169eb34040ba5dfb69d6223ad23a16140
Lena Mangold <lena@mangold.org>
2021-01-27 10:11:41 -0500

    update dspin_get

--- R/dspin_get.R
+++ R/dspin_get.R
@@ -1,6 +1,20 @@
 #' @export
-dspin_get <- function(slug, bucket_id, board = NULL, cache = TRUE, extract = NULL, version = NULL, files = FALSE, signature = NULL, ...) {
-  name <- paste0(bucket_id, "/", slug)
+dspin_get <- function(name, board = NULL, cache = TRUE, extract = NULL, version = NULL, files = FALSE, signature = NULL, ...) {
+
+  args <- list(...)
+  folder <- args$folder
+  bucket_id <- args$bucket_id
+
+  if(is.null(bucket_id)){
+    message("No bucket_id specified. Using 'user.dskt.ch' by default.")
+    bucket_id <- "user"
+  }
+
+  if(is.null(folder)){
+    stop("Need a folder to retrieve pin.")
+  }
+
+  board <- board_name(bucket_id, folder)
 
   pin_get(name = name,
           board = board,
