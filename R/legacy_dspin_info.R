cfef27bb0260d74dcd41c234a4e7beec5474c848
Lena Mangold <lena@mangold.org>
2021-08-16 06:29:38 -0500

    remove legacy files

--- R/legacy_dspin_info.R
+++
@@ -1,7 +0,0 @@
-
-#' @export
-dspin_info <- function(name, bucket_id, extended = FALSE){
-  .Deprecated("dspin_meta")
-  pins::pin_info(name = name, board = board_name(bucket_id),
-                 extended = extended, metadata = TRUE)
-}
